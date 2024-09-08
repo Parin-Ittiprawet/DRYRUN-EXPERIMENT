40087c008085716b66c93b498ad8c7ac76599ded
Parin-It <parin.i@doppiotech.com>
2024-09-09 01:15:51 +0700

    Change repo in groovy

--- common.groovy
+++ common.groovy
@@ -1,7 +1,7 @@
 def updateGithubStatus(state) {
     withCredentials([usernamePassword(credentialsId: 'github_dryrun_experiment', usernameVariable: 'USER', passwordVariable: 'PASSWORD')]) {
         sh """
-            curl --location 'https://api.github.com/repos/${USER}/Robot-Playground/statuses/${GIT_COMMIT}' \
+            curl --location 'https://api.github.com/repos/${USER}/DRYRUN-EXPERIMENT/statuses/${GIT_COMMIT}' \
                 --header 'Content-Type: application/json' \
                 --header 'Authorization: Bearer ${PASSWORD}' \
                 --data '{"state": "${state}", "context": "Dryrun", "description": "Jenkins", "target_url": "${BUILD_URL}"}'
